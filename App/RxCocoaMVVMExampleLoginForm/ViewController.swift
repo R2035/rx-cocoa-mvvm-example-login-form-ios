e7d570695141a684cdd63f248bde7eab8c66d164
R2035 <r2035.develop@gmail.com>
2021-10-19 08:45:57 +0900

    IBOutletを実装

--- App/RxCocoaMVVMExampleLoginForm/ViewController.swift
+++ App/RxCocoaMVVMExampleLoginForm/ViewController.swift
@@ -8,6 +8,12 @@
 import UIKit
 
 final class ViewController: UITableViewController {
+    @IBOutlet private var idTextField: UITextField!
+
+    @IBOutlet private var passwordTextField: UITextField!
+
+    @IBOutlet private var loginButton: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
