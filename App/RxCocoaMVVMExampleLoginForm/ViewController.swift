c49843d03e07556f4923314a7d7a0f7ac27e890b
R2035 <r2035.develop@gmail.com>
2021-10-19 08:11:53 +0900

    プロジェクトを作成

---
+++ App/RxCocoaMVVMExampleLoginForm/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RxCocoaMVVMExampleLoginForm
+//
+//  Created by oe on 2021/10/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
